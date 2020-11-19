c1a723473e2872c2aeebcc98cca511a76adee192
veilvokay <romayakovlev@hotmail.com>
2020-11-19 18:53:33 +0200

    update. 4/5

--- app/js/main.js
+++ app/js/main.js
@@ -82,5 +82,12 @@ function setItems(product) {
 }
 
 function totalCost(product) {
-    localStorage.setItem('totalCost', product.price);
+    let cartCost = localStorage.getItem('totalCost');
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price)
+    } else {
+        localStorage.setItem('totalCost', product.price);
+    }
 }
